f4b683a5de5e6dd2b0ed18a479330064709f901f
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-03-25 19:57:49 -0400

    first commit
    submit

--- index.js
+++ index.js
@@ -1,51 +1,74 @@
-function newImage(url, left, bottom){
-    let image = document.createElement('img')
-    image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
-    document.body.append(image)
-    return image
+function newImage(url) {
+  let image = document.createElement("img");
+  image.src = url;
+  document.body.append(image);
+  return image;
 }
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
-
-
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
-    item.addEventListener('click', () => {
-        item.remove()
-        let inventoryItem = document.createElement('img')
-        inventoryItem.src = url;
-        inventory.append(inventoryItem)
-    })
-    return item
+function move(image) {
+  image.style.position = "fixed";
+  image.style.left = left + "px";
+  image.style.bottom = bottom = "px";
+  //moving the character
+  function moveToCoordinates(left, bottom) {
+    image.style.left = left + "px";
+    image.style.bottom = bottom + "px";
+  }
+  return {
+    to: moveToCoordinates,
+  };
 }
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
-
-function newInventory(){
-    let inventory = document.createElement('div')
-    inventory.style.position = 'fixed'
-    inventory.style.bottom = '0px';
-    inventory.style.left = '0px'
-    inventory.style.width = '100%'
-    inventory.style.height = '100px'
-    inventory.style.display = 'flex'
-    inventory.style.flexDirection = 'row'
-    inventory.style.alignItems = 'center'
-    inventory.style.justifyContent = 'space-evenly'
-    inventory.style.border = '2px solid black'
-    inventory.style.backgroundColor = 'brown'
-    document.body.append(inventory)
-    return inventory
+thingsThatMoveReturned.to;
+function moveToCoordinates(left, bottom) {
+  image.style.left = left + "px";
+  image.style.bottom = bottom + "px";
 }
 
-const inventory = newInventory()
+let greenCharacter = newImage("assets/green-character.gif");
+move(greenCharacter).to(100, 250);
+
+let tree = newImage("assets/tree.png");
+move(tree, 200, 450);
+let pillar = newImage("assets/pillar.png");
+move(pillar, 350, 250);
+let pineTree = newImage("assets/pine-tree.png");
+move(pineTree, 450, 350);
+let crate = newImage("assets/crate.png");
+move(crate, 150, 350);
+let well = newImage("assets/well.png");
+move(well, 500, 575);
+
+function newItem(url, left, bottom) {
+  let item = newImage(url, left, bottom);
+  item.addEventListener("click", () => {
+    item.remove();
+    let inventoryItem = document.createElement("img");
+    inventoryItem.src = url;
+    inventory.append(inventoryItem);
+  });
+  return item;
+}
+
+newItem("assets/sword.png", 500, 555);
+newItem("assets/shield.png", 165, 335);
+newItem("assets/staff.png", 600, 250);
+
+function newInventory() {
+  let inventory = document.createElement("div");
+  inventory.style.position = "fixed";
+  inventory.style.bottom = "0px";
+  inventory.style.left = "0px";
+  inventory.style.width = "100%";
+  inventory.style.height = "100px";
+  inventory.style.display = "flex";
+  inventory.style.flexDirection = "row";
+  inventory.style.alignItems = "center";
+  inventory.style.justifyContent = "space-evenly";
+  inventory.style.border = "2px solid black";
+  inventory.style.backgroundColor = "brown";
+  document.body.append(inventory);
+  return inventory;
+}
+
+const inventory = newInventory();
