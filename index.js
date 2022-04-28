35f9e655d97be5abe6dd4221d6c92b654941e7fa
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-04-28 09:16:21 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,22 @@
-function newImage(url) {
+function newImage(url, left, bottom) {
   let image = document.createElement("img");
   image.src = url;
+  image.style.position = "fixed";
+  image.style.left = left + "px";
+  image.style.bottom = bottom + "px";
   document.body.append(image);
   return image;
 }
 
-function move(image) {
+newImage("assets/green-character.gif", 100, 250);
+newImage("assets/tree.png", 200, 450);
+newImage("assets/pillar.png", 350, 250);
+newImage("assets/pine-tree.png", 450, 350);
+newImage("assets/crate.png", 150, 350);
+newImage("assets/well.png", 500, 575);
+
+function newItem(url) {
   let item = newImage(url);
-  item.addEventListener("click", () => {
-    item.remove();
-    let inventoryItem = document.createElement("img");
-    inventoruItem.src = urlinventory.append(inventoryItem);
-  });
-  return item;
-}
-
-thingsThatMoveReturned.to;
-function moveToCoordinates(left, bottom) {
-  image.style.left = left + "px";
-  image.style.bottom = bottom + "px";
-}
-
-let greenCharacter = newImage("assets/green-character.gif");
-move(greenCharacter).to(100, 250);
-let tree = newImage("assets/tree.png");
-move(tree, 200, 450);
-let pillar = newImage("assets/pillar.png");
-move(pillar, 350, 250);
-let pineTree = newImage("assets/pine-tree.png");
-move(pineTree, 450, 350);
-let crate = newImage("assets/crate.png");
-move(crate, 150, 350);
-let well = newImage("assets/well.png");
-move(well, 500, 575);
-
-function newItem(url, left, bottom) {
-  let item = newImage(url, left, bottom);
   item.addEventListener("click", () => {
     item.remove();
     let inventoryItem = document.createElement("img");
@@ -51,9 +32,6 @@ newItem("assets/staff.png", 600, 250);
 
 function newInventory() {
   let inventory = document.createElement("div");
-  inventory.style.position = "fixed";
-  inventory.style.bottom = "0px";
-  inventory.style.left = "0px";
   inventory.style.width = "100%";
   inventory.style.height = "100px";
   inventory.style.display = "flex";
@@ -67,3 +45,89 @@ function newInventory() {
 }
 
 const inventory = newInventory();
+
+/*move(inventory).to(0, 0);
+//move(newImage("assets/green-character.gif")).to(100, 250);
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);*/
+
+//creating the charcter, track it with variable for current mosition
+const character = newImage("assets/green-character/static.gif");
+let direction = null;
+let x = 100;
+let y = 250;
+
+// to run code repeatedly using this function
+setInterval(function () {
+  if (direction === "west") {
+    x = x - 1;
+  }
+  if (direction === "north") {
+    y = y + 1;
+  }
+  if (direction === "east") {
+    x = x + 1;
+  }
+  if (direction === "south") {
+    y = y - 1;
+  }
+  character.style.left = x + "px";
+  character.style.bottom = y + "px";
+}, 1);
+
+// call back function , check which key is being pressed by e.key in each direction.
+//key down fires many times as long as the user is holding down the arrow key-repearing the event
+document.addEventListener("keydown", function (e) {
+  if (e.repeat) return;
+
+  if (e.key === "ArrowLeft") {
+    direction = "west";
+  }
+  if (e.key === "ArrowUp") {
+    direction = "north";
+  }
+  if (e.key === "ArrowRight") {
+    direction = "east";
+  }
+  if (e.key === "ArrowDown") {
+    direction = "south";
+  }
+  callback(direction);
+});
+
+document.addEventListener("keyup", function (e) {
+  direction = null;
+  callback(direction);
+});
+
+// so character can appear to be walking, chaning animations.
+function handleDirectionChange(direction) {
+  if (direction === null) {
+    character.src = "assets/green-character/static.gif";
+  }
+  if (direction === "west") {
+    character.src = "assets/green-character/west.gif";
+  }
+  if (direction === "north") {
+    character.src = "assets/green-character/north.gif";
+  }
+  if (direction === "east") {
+    character.src = "assets/green-character/east.gif";
+  }
+  if (direction === "south") {
+    character.src = "assets/green-character/south.gif";
+  }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange);
+
+// event listener so that the character stops moving when the user releases the arrow key
+document.addEventListener("keyup", function (e) {
+  direction = null;
+});
